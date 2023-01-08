c92ea284e923f2ea4c139c0393eae020d5297ed7
umair <cyumair@hotmail.com>
2023-01-08 18:15:02 +0500

    adding 1st test case

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
   
 # instance of Options class allows
 # us to configure Headless Chrome
@@ -21,25 +23,24 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome(options=options)
  
     # Test case method. It should always start with test_
-    def test_search_in_python_org(self):
+    def test_empty_input_during_login(self):
          
         # get driver
         driver = self.driver
         # get python.org using selenium
-        driver.get("http://www.python.org")
- 
-        # assertion to confirm if title has python keyword in it
-        self.assertIn("Python", driver.title)
+        driver.get("https://web-production-b1d0.up.railway.app/")
  
         # locate element using name
-        # elem = driver.find_element_by_name("q")
- 
-        # send data
-        # elem.send_keys("pycon")
- 
-        # receive data
-        # elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        driver.find_element(By.NAME, "email").send_keys("")
+        driver.find_element(By.NAME, "password").send_keys("")
+        #click the button
+        driver.find_element(By.CSS_SELECTOR, "button").click()
+        #wait for 4 seconds
+        time.sleep(4)
+        #get the text of the error message
+        text = driver.find_element(By.CSS_SELECTOR, "#loginMessage").text
+        print(text);
+        self.assertEqual(text, "Invalid email or password")
  
     # cleanup method called after every test performed
     def tearDown(self):
